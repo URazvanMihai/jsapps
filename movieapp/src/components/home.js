949dac5c51988231a22faf7cce8ebc47c03d1bbb
Mihai Razvan <mihairazvanuuu@gmail.com>
2022-10-27 01:00:39 +0300

    Header Done Backround Problem

--- movieapp/src/components/home.js
+++ movieapp/src/components/home.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import styled from"styled-components"
 
 function home() {
   return (
-    <div>home</div>
+    <Container>
+      home
+    </Container>
   )
 }
 
 export default home
+
+const Container = styled.main`
+  min-hight: calc(100vh - 70px);
+  padding: 0 calc(3.5vw + 5px);
+  position: relative;
+  
+  &:before {
+    background: url("/images/home-background.png")
+    center center / cover no-repeat fixed;
+    background-size:auto;
+    content:"";
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: -1;
+  }
+`
